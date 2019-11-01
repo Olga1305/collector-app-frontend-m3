28980840937fd49979a309302c5a8547ecd8847c
Olga Dokukova <dokukova@gmail.com>
2019-11-01 15:26:17 +0100

    [refactor] close #15 refactor subbrand view

--- src/views/DollsList.js
+++ src/views/DollsList.js
@@ -8,9 +8,6 @@ import InfoBox from '../components/InfoBox';
 class DollsList extends Component {
   state = {
     dolls: [],
-    years: [],
-    collections: [],
-    finalCollections: [],
     loading: true,
     brand: undefined,
     subBrand: undefined,
@@ -36,25 +33,10 @@ class DollsList extends Component {
       this.setState({
         dolls,
         brand,
-        subBrand,
+        subBrand,      
+        loading: false,
       });
-      try {
-        this.findYearsAndCollections();
-        try {
-          this.findFinalCollections();
-          console.log(this.state);
-        } catch (error) {
-          console.log(error);
-          this.setState({
-            loading: false,
-          });
-        }
-      } catch (error) {
-        console.log(error);
-        this.setState({
-          loading: false,
-        });
-      }
+      
     } catch (error) {
       console.log(error);
       this.setState({
@@ -63,109 +45,12 @@ class DollsList extends Component {
     }
   }
 
-  findYearsAndCollections = () => {
-    const { dolls } = this.state;
-    const allYears = [];
-    const years = [];
-    const allCollections = [];
-    const collections = [];
-
-    dolls.forEach(doll => {
-      if (doll.year !== '') {
-        allYears.push(doll.year);
-      } else {
-        return allYears;
-      }
-    });
-
-    if (allYears.length > 0) {
-      allYears.forEach(item => {
-        if (years.indexOf(item) === -1) {
-          years.push(item);
-        }
-        return years;
-      });
-    }
-
-    dolls.forEach(doll => {
-      if (doll.collectionName !== '') {
-        allCollections.push(doll.collectionName);
-      } else {
-        return allCollections;
-      }
-    });
-
-    if (allCollections.length > 0) {
-      allCollections.forEach(item => {
-        if (collections.indexOf(item) === -1) {
-          collections.push(item);
-        }
-        return collections;
-      });
-    }
-
-    this.setState({
-      years,
-      collections,
-    });
-  };
-
-  // findDollsByYear = (year) => {
-  //   const { dolls } = this.state;
-  //   return dolls.filter(doll => {
-  //       return doll.year === year;
-  //     });
-  // }
-
-  findCollectionsByYear = (arr, year) => {
-    const result = { year: year, yearColl: [] };
-    result.yearColl.push(
-      arr.filter(el => {
-        return el.collection[0][0].year === year;
-      }),
-    );
-
-    return result;
-  };
-
-  findDollsByCollection = collection => {
-    const { dolls } = this.state;
-    const result = { collName: collection, collection: [] };
-    result.collection.push(
-      dolls.filter(doll => {
-        return doll.collectionName.includes(collection);
-      }),
-    );
-    return result;
-  };
-
-  findFinalCollections = () => {
-    const { years, collections } = this.state;
-    const dollsByCollection = [];
-    const collectionsByYear = [];
-
-    collections.forEach(collection => {
-      dollsByCollection.push(this.findDollsByCollection(collection));
-    });
-
-    years.forEach(year => {
-      collectionsByYear.push(this.findCollectionsByYear(dollsByCollection, year));
-    });
-
-    // console.log(dollsByCollection)
-    // console.log(collectionsByYear)
-
-    this.setState({
-      finalCollections: collectionsByYear,
-      loading: false,
-    });
-  };
 
   render() {
-    const { brand, subBrand, finalCollections, loading } = this.state;
-    let options = [];
+    const { brand, subBrand, dolls, loading } = this.state;
+    const options = [];
 
-    finalCollections.forEach((el, index) => {
+    dolls.forEach((el, index) => {
       options.push(<h2 key={`${el.year}-${index}`}>{el.year}</h2>);
       el.yearColl.forEach(coll => {
         coll.forEach(name => {
