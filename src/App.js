7c1a1ad924ee5cf286ef731441f5a7da78a63ed2
Olga Dokukova <dokukova@gmail.com>
2019-11-02 02:30:08 +0100

    [feat] close #9 create profile page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Navbar from './components/Navbar';
 import Catalog from './views/Catalog';
 import DollsList from './views/DollsList';
 import DollDetail from './views/DollDetail';
+import Profile from './views/user/Profile';
 
-import PrivateView from './views/PrivateView';
 import Login from './views/auth/Login';
 import Signup from './views/auth/Signup';
 import { withAuth } from './Context/AuthContext';
@@ -20,7 +20,7 @@ import AnonRoute from './components/AnonRoute';
 
 class App extends Component {
 
-  // eslint-disable-next-line class-methods-use-this
+  
   render() {
     const { handleLogout } = this.props;
     
@@ -30,15 +30,16 @@ class App extends Component {
           
               <Router>
                 <Navbar/>
-                <button className="logout" onClick={handleLogout}>logout</button>
+                
                 <div className="content-wrap">
                   <Route exact path="/" component={Catalog} />
                   <AnonRoute exact path="/login" component={Login} />
                   <AnonRoute exact path="/signup" component={Signup} />
                   <Route exact path="/catalog" component={Catalog} />                 
                   <Route exact path="/catalog/:brand" component={DollsList} />                    
-                  <Route exact path="/catalog/:brand/:id" component={DollDetail} />
-                  <PrivateRoute exact path="/private" component={PrivateView} />  
+                  <Route exact path="/catalog/:brand/:id" component={DollDetail} />                
+                  <PrivateRoute exact path="/profile" component={Profile} />  
+                  <button className="logout" onClick={handleLogout}>logout</button>
                  
                 </div>     
                       
---
+++ src/views/user/Profile.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Profile.css';
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>My profile</h1>
+      <Link className="button" to="/mycollection">
+        My collection
+      </Link>
+      <br />
+      <Link className="button" to="/mywishlist">
+        My wishlist
+      </Link>
+      <br />
+    </div>
+  );
+};
+
+export default Profile;
