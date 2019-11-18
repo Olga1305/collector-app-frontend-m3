63f9999d45e2c4a74162dee3f4de5939449a51b1
Olga Dokukova <dokukova@gmail.com>
2019-11-19 00:55:59 +0100

    auth forms background

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -27,34 +27,43 @@ class Login extends Component {
   render() {
     const { email, password } = this.state;
     return (
-      <div className="auth-container">
-        <form id="auth" onSubmit={this.handleFormSubmit}>
-          <div className="header">
-            <img className="auth-logo" src={logo} alt="logo" />
-            <h1>Log in</h1>
-            <p>Log in to see your collection</p>
-          </div>
-          <div className="sep"></div>
-          <div className="inputs">
-            <input type="email" placeholder="Email" name="email" value={email} onChange={this.handleChange} autoFocus />
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={password}
-              autoComplete="current-password"
-              onChange={this.handleChange}
-            />
+      <div className="auth-body">
+        <div className="auth-container">
+          <form id="auth" onSubmit={this.handleFormSubmit}>
+            <div className="header">
+              <img className="auth-logo" src={logo} alt="logo" />
+              <h1>Log in</h1>
+              <p>Log in to see your collection</p>
+            </div>
+            <div className="sep"></div>
+            <div className="inputs">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+                autoFocus
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                autoComplete="current-password"
+                onChange={this.handleChange}
+              />
 
-            <input id="submit" type="submit" value="Log in" />
-          </div>
-          <p className="auth-link">
-            Don't have account?{' '}
-            <Link className="auth-link" to={'/signup'}>
-              Sign up
-            </Link>
-          </p>
-        </form>
+              <input id="submit" type="submit" value="Log in" />
+            </div>
+            <p className="auth-link">
+              Don't have account?{' '}
+              <Link className="auth-link" to={'/signup'}>
+                Sign up
+              </Link>
+            </p>
+          </form>
+        </div>
       </div>
     );
   }
