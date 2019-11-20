0e181580f825a6b255a7b0dc820dd21ddcda3c24
Olga Dokukova <dokukova@gmail.com>
2019-11-20 18:58:31 +0100

    update

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import SweetAlert from 'react-bootstrap-sweetalert';
-import { Link } from 'react-router-dom';
 import { withAuth } from '../../Context/AuthContext';
 import LoginForm from './LoginForm';
 import './Auth.css';
@@ -73,45 +72,6 @@ class Login extends Component {
 
       </>
 
-
-      // <div className="auth-body">
-      //   <div className="auth-container">
-      //     <form id="auth" onSubmit={this.handleFormSubmit}>
-      //       <div className="header">
-      //         <img className="auth-logo" src={logo} alt="logo" />
-      //         <h1>Log in</h1>
-      //         <p>Log in to see your collection</p>
-      //       </div>
-      //       <div className="sep"></div>
-      //       <div className="inputs">
-      //         <input
-      //           type="email"
-      //           placeholder="Email"
-      //           name="email"
-      //           value={email}
-      //           onChange={this.handleChange}
-      //           autoFocus
-      //         />
-      //         <input
-      //           type="password"
-      //           placeholder="Password"
-      //           name="password"
-      //           value={password}
-      //           autoComplete="current-password"
-      //           onChange={this.handleChange}
-      //         />
-
-      //         <input id="submit" type="submit" value="Log in" />
-      //       </div>
-      //       <p className="auth-link">
-      //         Don't have account?{' '}
-      //         <Link className="auth-link" to={'/signup'}>
-      //           Sign up
-      //         </Link>
-      //       </p>
-      //     </form>
-      //   </div>
-      // </div>
     );
   }
 }
